88f05d16ddff4cb5007656a92c13d09b77f6f16a
SultanNarynbekU <110475786+SultanNarynbekU@users.noreply.github.com>
2023-10-20 21:08:28 +0600

    lorem

--- src/main/week1/SequenceIJ4.java
+++ src/main/week1/SequenceIJ4.java
@@ -1,13 +1,14 @@
 public class SequenceIJ4 {
     public static void main(String[] args) {
         for (double i = 0.0 ; i < 2.1 ; i += 0.2) {
-            for (double j = 0.1; j <= 3; j++) {
-                if (i * 10 % 10 == 0) {
-                    int a =(int) i;
-                    int b = (int) (i+j);
-                    System.out.printf("I=%d J=%d\n",a,b);
-                } else {
+            for (double j = 1; j <= 3; j++) {
+                if (i > 0 && i < 1 || i > 1 && i < 2) {
                     System.out.printf("I=%.1f J=%.1f\n",i,i+j);
+
+                } else {
+                    int a =(int) i;
+                    int b = (int) (i + j);
+                    System.out.printf("I=%d J=%d\n",a,b);
 
                 }
             }
