8cddeaca0c953e13d43869bb31d7184879d8476a
Azamat <119999322+kitarooo@users.noreply.github.com>
2023-10-27 19:10:19 +0600

    Add foreign and primary key

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,12 +1,12 @@
 CREATE TABLE cars (
-                          car_id integer,
+                          car_id integer PRIMARY KEY,
                           car_name varchar(30),
                           car_brand varchar (30)
                           car_price int
                   );
 
 CREATE TABLE clients (
-                         client_id int ,
+                         client_id int PRIMARY KEY,
                          client_name varchar(50)
                      );
 
@@ -14,4 +14,6 @@ CREATE TABLE orders (
                            orders_id int PRIMARY KEY,
                            client_id int,
                            car_id int,
+                           FOREIGN KEY (client_id) REFERENCES clients (client_id),
+                           FOREIGN KEY (car_id) REFERENCES cars (car_id)
 );
